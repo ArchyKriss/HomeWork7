5ab268aa35e30ef258e258ac07bb9b6e5785ad92
user <git@github.com:ArchyKris/helloWord.git>
2024-07-29 22:09:41 +0300

    Исправлено ДЗ

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,22 @@ public class Main {
         int sum = 0;
         int accum = 15_000;
         int month = 0;
-        while(sum<=2_459_000){
+        while (sum <= 2_459_000) {
             month++;
-            sum+=accum;
-            sum+=sum*percent;
-            System.out.println("Месяц "+month+", сумма накоплений равна "+sum+" рублей");
+            sum += accum;
+            sum += sum * percent;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + sum + " рублей");
         }
 
         //task2
         int numForWhile = 0;
-        while (numForWhile<10){
+        while (numForWhile < 10) {
             numForWhile++;
-            System.out.print(numForWhile+" ");
+            System.out.print(numForWhile + " ");
         }
         System.out.println();
-        for(int i =10; i>=1;i--){
-            System.out.print(i+" ");
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
         }
         System.out.println();
 
@@ -28,39 +28,35 @@ public class Main {
         int population = 12_000_000;
         int birth = 17;
         int death = 8;
-        int people = birth-death;
-        int populationYear = population/1000*people;
-        int year = 0;
-        while(year<10){
-            population+=populationYear;
-            year++;
-            System.out.println("Год "+year+", численность населения составляет "+population);
+        for (int i = 1; i <= 10 ; i++) {
+            population += population/1000*(birth-death);
+            System.out.println("Год " + i + ", численность населения составляет " + population);
         }
 
         //task4
         int initialDeposit = 15_000;
-        int sevenPercentFromDeposit = initialDeposit/100*7;
+        int sevenPercentFromDeposit = initialDeposit / 100 * 7;
         int needSum = initialDeposit;
         int month2 = 0;
-        while(needSum<12_000_000){
+        while (needSum < 12_000_000) {
             month2++;
-            needSum+=sevenPercentFromDeposit;
+            needSum += sevenPercentFromDeposit;
             if (month2 % 6 == 0) {//добавленое условие из 5ой задачи
-            System.out.println("Месяц "+month2+" Сумма накоплений равна "+needSum);
-        }
+                System.out.println("Месяц " + month2 + " Сумма накоплений равна " + needSum);
+            }
         }
 
         //task6
         int initialDeposit1 = 15_000;
-        int persents = initialDeposit1/100*7;
+        int persents = initialDeposit1 / 100 * 7;
         int accumSum = initialDeposit1;
-        int totalMonths = 9*12;
+        int totalMonths = 9 * 12;
         int months = 0;
-        while (months<totalMonths){
-            accumSum+=persents;
+        while (months < totalMonths) {
+            accumSum += persents;
             months++;
-            if(months%6==0){
-                System.out.println("Месяц "+months+ ", Сумма накоплений равна "+accumSum);
+            if (months % 6 == 0) {
+                System.out.println("Месяц " + months + ", Сумма накоплений равна " + accumSum);
             }
         }
 
@@ -76,9 +72,10 @@ public class Main {
         int before = 2024 - 200;
         int after = 2024 + 100;
 
-        for (int i = before; i < after; i ++) {
-            if (i % 79 == 0){
-                System.out.println(i);}
+        for (int i = before; i < after; i++) {
+            if (i % 79 == 0) {
+                System.out.println(i);
+            }
         }
 
     }
